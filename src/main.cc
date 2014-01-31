368228240500abe45b5bf299dd16b03c30d519e6
Brian Brooks <brooks.brian@gmail.com>
2014-01-31 15:59:15 -0500

    main.cc command_line

--- src/main.cc
+++ src/main.cc
@@ -1,2 +1,15 @@
-int main(void) {}
+#include <iostream>
 
+#include "command_line.h"
+#include "file_path.h"
+
+int main(int argc, const char* argv[]) {
+  CommandLine cl(argc, argv);
+
+  if (!cl.HasSwitch("dir")) {
+    std::cout << "usage: " << argv[0] << " --dir=<music-directory>" << std::endl;
+    return 0;
+  }
+  base::FilePath dir = cl.GetSwitchValuePath("dir");
+  std::cout << "dir=" << dir.value() << std::endl;
+}
