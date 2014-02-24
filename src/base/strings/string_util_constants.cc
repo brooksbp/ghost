e010de45082952db51df3abd3e14539e98267a12
Brian Brooks <brooks.brian@gmail.com>
2014-02-23 20:54:09 -0500

    whitespace

--- src/base/strings/string_util_constants.cc
+++ src/base/strings/string_util_constants.cc
@@ -4,12 +4,12 @@
 
 namespace base {
 
-#define WHITESPACE_UNICODE                      \
+#define WHITESPACE_UNICODE                              \
   0x0009, /* <control-0009> to <control-000D> */        \
-  0x000A, \
-  0x000B, \
-  0x000C, \
-  0x000D, \
+  0x000A,                                             \
+  0x000B,                                             \
+  0x000C,                                           \
+  0x000D,                                           \
   0x0020, /* Space */                                 \
   0x0085, /* <control-0085> */                        \
   0x00A0, /* No-Break Space */                        \
@@ -34,24 +34,24 @@ namespace base {
   0x3000, /* Ideographic Space */                     \
   0
 
-  const wchar_t kWhitespaceWide[] = {
-    WHITESPACE_UNICODE
-  };
+const wchar_t kWhitespaceWide[] = {
+  WHITESPACE_UNICODE
+};
 
-  const char16 kWhitespaceUTF16[] = {
-    WHITESPACE_UNICODE
-  };
+const char16 kWhitespaceUTF16[] = {
+  WHITESPACE_UNICODE
+};
 
-  const char kWhitespaceASCII[] = {
-    0x09,    // <control-0009> to <control-000D>
-    0x0A,
-    0x0B,
-    0x0C,
-    0x0D,
-    0x20,    // Space
-    0
-  };
+const char kWhitespaceASCII[] = {
+  0x09,    // <control-0009> to <control-000D>
+  0x0A,
+  0x0B,
+  0x0C,
+  0x0D,
+  0x20,    // Space
+  0
+};
 
-  const char kUtf8ByteOrderMark[] = "\xEF\xBB\xBF";
+const char kUtf8ByteOrderMark[] = "\xEF\xBB\xBF";
 
 }  // namespace base
