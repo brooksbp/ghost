68ae1435c25dab566236c96a457429374c556184
Brian Brooks <brooks.brian@gmail.com>
2014-03-30 10:04:25 -0400

    - Add PlayerUi

---
+++ src/player_ui.cc
@@ -0,0 +1,32 @@
+// Copyright 2014 Ghost Authors. All rights reserved.
+// Use of this source code is governed by a ALv2 license that can be
+// found in the LICENSE file.
+
+#include "player_ui.h"
+
+static int dummy_argc = 1;
+static char* dummy_argv = const_cast<char*>("ghost");
+
+PlayerUi::PlayerUi() {
+  app = new QApplication(dummy_argc, &dummy_argv);
+
+  main_window = new MainWindow();
+
+  LOG(INFO) << "PlayerUi()";
+}
+
+PlayerUi::~PlayerUi() {
+  LOG(INFO) << "~PlayerUi()";
+  app->closeAllWindows();
+
+  main_window->Shutdown();
+  // FIXME(brbrooks) dont leak mainwindow
+  //main_window = NULL;
+
+  //delete app;
+  LOG(INFO) << "~PlayerUi()";
+}
+
+scoped_refptr<MainWindow> PlayerUi::GetMainWindow() {
+  return main_window;
+}
---
+++ src/player_ui.h
@@ -0,0 +1,28 @@
+// Copyright 2014 Ghost Authors. All rights reserved.
+// Use of this source code is governed by a ALv2 license that can be
+// found in the LICENSE file.
+
+#ifndef PLAYER_UI_H_
+#define PLAYER_UI_H_
+
+#include "main_window.h"
+
+#include "base/logging.h"
+#include "base/memory/ref_counted.h"
+
+#include <QtWidgets/QApplication>
+
+class PlayerUi : public base::RefCountedThreadSafe<PlayerUi> {
+ public:
+  PlayerUi();
+  ~PlayerUi();
+
+  // FIXME(brbrooks) we are just leaking here...
+  scoped_refptr<MainWindow> GetMainWindow();
+
+ private:
+  QApplication* app;
+  scoped_refptr<MainWindow> main_window;
+};
+
+#endif  // PLAYER_UI_H_
